1f4b638b775c42a15c5dacd0beb44c1507ebbb2a
Rubén Borbolla <53828155+rubenbc7@users.noreply.github.com>
2020-02-02 02:28:52 -0700

    Add files via upload

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -11,13 +11,19 @@ public class TimerScript : MonoBehaviour
     float TimeLeft ;
     public float MaxTime = 20f;
     float pipi = 0.3045f;
+    public int IsRunning=1;
+    public int numerorandom;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        numerorandom = UnityEngine.Random.Range(0, 6);
+        Debug.Log(numerorandom);
         timer = GetComponent<Image>();
         TimeLeft = MaxTime;
+
+        StartCoroutine(NumberGen());
     }
 
     // Update is called once per frame
@@ -26,7 +32,9 @@ public class TimerScript : MonoBehaviour
         if (TimeLeft > 0)
         {
             TimeLeft -= Time.deltaTime;
-            timer.transform.Rotate(0, 0, -pipi); 
+            timer.transform.Rotate(0, 0, -pipi);
+
+ 
         }
 
         if(TimeLeft<=0)
@@ -34,6 +42,51 @@ public class TimerScript : MonoBehaviour
            
             SceneManager.LoadScene(1);
         }
+
+ 
     }
-    
+
+    IEnumerator NumberGen()
+    {
+        while (true)
+        {  IsRunning = 0;
+           
+            yield return new WaitForSeconds(2);
+            
+
+            switch (numerorandom)
+            {
+                case 0:
+                    Debug.Log("Barril incendiado");
+                    break;
+                case 1:
+                    Debug.Log("Hoyo en el barco");
+                    break;
+                case 2:
+                    Debug.Log("Timon caido");
+                    break;
+                case 3:
+                    Debug.Log("Fuego en la proa");
+                    break;
+                case 4:
+                    Debug.Log("valla caida");
+                    break;
+                case 5:
+                    Debug.Log("valla caida dos");
+                    break;
+                default:
+                    break;
+
+            }
+
+            numerorandom = UnityEngine.Random.Range(0, 6);
+            Debug.Log(numerorandom);
+
+            IsRunning = 1;
+}
+    }
+
+    //Put this in Start()
+
+
 }
