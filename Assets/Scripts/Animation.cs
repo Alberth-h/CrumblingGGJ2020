4738d75463c5aeb759d25b977b31cd3ace734ca9
Alberto Hoyos <53828141+Alberth-h@users.noreply.github.com>
2020-02-02 01:27:15 -0700

    Script de animacion xd

---
+++ Assets/Scripts/Animation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    public float horizontalMove;
+    public float verticalMove;
+    private Vector3 playerInput;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+
+        playerInput = new Vector3(horizontalMove, 0, verticalMove);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+
+        if (horizontalMove != 0 || verticalMove != 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+}
