a93d4cb31f3865d4098a967d6489a1805f27ae53
Rubén Borbolla <53828155+rubenbc7@users.noreply.github.com>
2020-02-02 04:17:07 -0700

    Add files via upload

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -14,10 +14,35 @@ public class TimerScript : MonoBehaviour
     public int IsRunning=1;
     public int numerorandom;
 
+    public GameObject fire;
+    public GameObject water;
+    public GameObject timon;
+    public GameObject proa;
+    public GameObject valla;
+    public GameObject valla2;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        fire = GameObject.Find("FireBarrel");
+        fire.SetActive(false);
+
+        water = GameObject.Find("WaterHole");
+        water.SetActive(false);
+
+        timon = GameObject.Find("rueda");
+        timon.transform.Rotate(0, 0, 0);
+        timon.transform.position = new Vector3(0, 3, 12);
+
+        proa = GameObject.Find("FireProa");
+        proa.SetActive(false);
+
+        valla = GameObject.Find("/Boat/Fences/Fence06");
+
+        valla = GameObject.Find("/Boat/Fences/Fence09");
+
+
         numerorandom = UnityEngine.Random.Range(0, 6);
         Debug.Log(numerorandom);
         timer = GetComponent<Image>();
@@ -58,21 +83,29 @@ public class TimerScript : MonoBehaviour
             {
                 case 0:
                     Debug.Log("Barril incendiado");
+                    fire.SetActive(true);
                     break;
                 case 1:
                     Debug.Log("Hoyo en el barco");
+                    water.SetActive(true);
                     break;
                 case 2:
                     Debug.Log("Timon caido");
+                    timon.transform.Rotate(0, 90, -91);
+                    timon.transform.position = new Vector3(0, 1.7f, 10);
                     break;
                 case 3:
                     Debug.Log("Fuego en la proa");
+                    proa.SetActive(true);
                     break;
                 case 4:
                     Debug.Log("valla caida");
+                    valla.transform.Rotate(40, 20, 0);
+                    
                     break;
                 case 5:
                     Debug.Log("valla caida dos");
+                    valla.transform.Rotate(40, 20, 0);
                     break;
                 default:
                     break;
