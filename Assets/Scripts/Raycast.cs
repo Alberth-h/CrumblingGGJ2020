9723a87b06df48a749d38eb4d3a11e02d0ddafa1
Carlos Encinas <carlos-encinas@hotmail.com>
2020-02-02 02:24:43 -0700

    avance

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Raycast : MonoBehaviour
 {
+
+    public GameObject player;
+    //public float xAngle;
+    //private float Anglex;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
+
+    [SerializeField]
+    Color rayColor = Color.red;
+    [SerializeField, Range(0f, 20f)]
+    public float rayDistance;
+
+    public RaycastHit TheHit;
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (CloseObject)
+        {
+            Debug.Log("Estás tocando algo.");
+        }
+    }
+
+    bool CloseObject
+    {
+        get => Physics.Raycast(player.transform.position, new Vector3(rayDistance, 0)); 
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = rayColor;
+        Gizmos.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * rayDistance);
     }
 }
