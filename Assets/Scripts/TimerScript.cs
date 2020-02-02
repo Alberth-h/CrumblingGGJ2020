8d4c28e9ae3cdf6cb91b249185d2bf83fcfb8d93
Rubén Borbolla <53828155+rubenbc7@users.noreply.github.com>
2020-02-01 19:54:19 -0700

    Add files via upload

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+
+    Image timer;
+    float TimeLeft ;
+    public float MaxTime = 20f;
+    float pipi = 0.3045f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = GetComponent<Image>();
+        TimeLeft = MaxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TimeLeft > 0)
+        {
+            TimeLeft -= Time.deltaTime;
+            timer.transform.Rotate(0, 0, -pipi); 
+        }
+
+        else
+        {
+            Time.timeScale = 0;
+        }
+    }
+    
+}
